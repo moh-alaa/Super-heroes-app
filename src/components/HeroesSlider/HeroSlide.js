7cf942d969285cbaa080750bea8be161f0e9554c
Max Abdelmonam <mohammedalaa159@gmail.com>
2021-08-28 14:33:01 +0800

    added single carousel item

--- src/components/HeroesSlider/HeroSlide.js
+++ src/components/HeroesSlider/HeroSlide.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import { Link } from "react-router-dom";
-
 import styles from "./HeroSlider.module.scss";
 
 
 export const HeroSlide = (props) => {
     const { hero, heroConfig } = props;
     return (
-
-    ) 
+        <div className={styles.slide}>
+            <div className={styles.heroInfo}>
+                <div className={styles.heroTitle}>
+                    <Link to={`/hero/${hero.id}`}>{hero.name}</Link>
+                </div>
+            </div>
+            <div className={styles.slideImageWrap}>
+                <img className={styles.slideImage} src={hero.image.url} />
+            </div>
+            <div className={styles.heroAttributes}>
+                {/* Heros attributes here */}
+            </div>
+        </div>
+    ); 
 }
